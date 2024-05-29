5473f87e2867f08408d5a777b2a4cfae4dab22ca
Ganesh Patil <gp28info@gmail.com>
2024-05-29 17:57:19 +0530

    EP 14 Angualr 17 Standalone Component

---
+++ crew-management/src/app/crew-desginations/crew-desginations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-desginations',
+  templateUrl: './crew-desginations.component.html',
+  styleUrl: './crew-desginations.component.scss'
+})
+export class CrewDesginationsComponent {
+
+}

23b4dff759f76d70851231a64fb8a6c4f28366d8
Ganesh Patil <gp28info@gmail.com>
2024-06-06 16:30:18 +0530

    Lazy Loading Component
    5.)   Make use of <router-outlet></router-outlet> in app.component.html

--- crew-management/src/app/app.routes.ts
+++ crew-management/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path:'admin',
         loadChildren:() =>   import('./admin/admin.module').then(m => m.AdminModule)
+    },
+    {
+        path:'add-new-user',
+        loadComponent:() =>   import('./admin/add-new-user/add-new-user.component').then(c => c.AddNewUserComponent)
     }
 ];
